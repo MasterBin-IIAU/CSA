e494a18ba9c77815ca7c2f4c4c738e3c05a0bc7d
masterbin-iiau <yan_bin@mail.dlut.edu.cn>
2020-03-19 08:07:49 +0800

    fix

--- GAN_utils_search_0.py
+++ GAN_utils_search_0.py
@@ -6,16 +6,13 @@ opt = TestOptions().parse()
 
 # modify some config
 '''Attack Search Regions'''
-opt.model = 'G_search_resize_512' # only cooling
-# opt.model = 'G_search_resize_512_regress' # cooling + shrinking
-# opt.model = 'G_search_L2_500'
-# opt.model = 'G_search_L2_500_regress'
+opt.model = 'G_search_L2_500' # only cooling
+# opt.model = 'G_search_L2_500_regress' # cooling + shrinking
 opt.netG = 'unet_256'
 
 # create and initialize model
 '''create perturbation generator'''
 GAN = create_model(opt)  # create a model given opt.model and other options
-# GAN.load_path = os.path.join(project_path_,'checkpoints/%s/1_net_G.pth'%opt.model)
-GAN.load_path = os.path.join('/home/masterbin-iiau/Desktop/AdvTrack-checkpoints','checkpoints/%s/1_net_G.pth'%opt.model)
+GAN.load_path = os.path.join(project_path_,'checkpoints/%s/1_net_G.pth'%opt.model)
 GAN.setup(opt)  # # regular setup: load and print networks; create schedulers
 GAN.eval()
--- GAN_utils_template_1.py
+++ GAN_utils_template_1.py
@@ -6,18 +6,14 @@ opt = TestOptions().parse()
 
 # modify some config
 '''Attack Template'''
-# opt.model = 'G_template_L2_1000' # only cooling
-opt.model = 'G_template_L2_1000_regress' # cooling + shrinking
-# opt.model = 'G_template_L2_500_regress' # only cooling
+# opt.model = 'G_template_L2_500' # only cooling
+opt.model = 'G_template_L2_500_regress' # cooling + shrinking
 opt.netG = 'unet_128'
 
 
 # create and initialize model
 '''create perturbation generator'''
 GAN = create_model(opt)  # create a model given opt.model and other options
-# GAN.load_path = os.path.join(project_path_,'checkpoints/%s/1_net_G.pth'%opt.model)
-# GAN.load_path = os.path.join('/home/masterbin-iiau/Desktop/AdvTrack-checkpoints','checkpoints/%s/1_net_G.pth'%opt.model)
-GAN.load_path = os.path.join('/home/masterbin-iiau/Desktop/AdvTrack-checkpoints','checkpoints/%s/latest_net_G.pth'%opt.model)
-
+GAN.load_path = os.path.join(project_path_,'checkpoints/%s/latest_net_G.pth'%opt.model)
 GAN.setup(opt)  # # regular setup: load and print networks; create schedulers
 GAN.eval()
